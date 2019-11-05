ea8e13b0456475594a942369bb3bc451c13e7b0d
jacobcalv <jacobcalvino@outlook.com>
2019-11-04 19:36:37 -0600

    finished sign in, friendslist, and some of protected route

---
+++ friends/src/components/Login.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react"
+import api from './utils/api'
+
+function Login(props) {
+    const [error, setError] = useState()
+    const [data, setData] = useState({
+        username: '',
+        password: '',
+    })
+
+    const handleChange = (event) => {
+		setData({
+			...data,
+			[event.target.name]: event.target.value,
+		})
+    }
+    
+	const handleSubmit = (event) => {
+        event.preventDefault()
+        
+		api()
+			.post("/api/login", data)
+			.then(result => {
+				localStorage.setItem("token", result.data.payload)
+				console.log(result.data)
+				props.history.push("/account")
+			})
+			.catch(err => {
+				setError(err.response.data.message)
+			})
+    }
+    
+    return (
+		<form onSubmit={handleSubmit}>
+			{error && <div className="error">{error}</div>}
+
+			<input type='text' name="username" placeholder="Username" value={data.username} onChange={handleChange} />
+			<input type="password" name="password" placeholder="Password" value={data.password} onChange={handleChange} />
+
+			<button type="submit">Sign In</button>
+		</form>
+	)
+}
+
+export default Login
